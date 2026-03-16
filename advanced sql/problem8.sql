59672be7b5a54f1d3d2416023ea95fcc85eac503
Hamza <hamzakhoja000@gmail.com>
2026-03-16 16:15:11 +0100

    push exmeple

---
+++ advanced sql/problem8.sql
@@ -0,0 +1,15 @@
+SELECT
+    quarter1.job_location,
+    quarter1.job_via,
+    quarter1.job_posted_date::DATE,
+    quarter1.salary_year_avg
+FROM (
+    SELECT * FROM january_jobs
+    UNION ALL
+    SELECT * FROM february_jobs
+    UNION ALL
+    SELECT * FROM march_jobs
+) AS quarter1
+WHERE quarter1.salary_year_avg > 70000
+  AND quarter1.job_title_short = 'Data Analyst'
+ORDER BY quarter1.salary_year_avg DESC;
